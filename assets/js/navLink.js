e3ff83b66f8dae2b8f8cc5670c2bda3dc232434c
William Schrader <wfschrad@gmail.com>
2020-06-05 21:12:15 -0700

    Adjust navlinks-1

---
+++ assets/js/navLink.js
@@ -0,0 +1,10 @@
+// Grab nav icons
+
+const gitHub = document.querySelector('.fa-github');
+const linkedIn = document.querySelector('.fa-linkedin');
+
+
+
+// Add listeners
+gitHub.addEventListener('click', () => window.open('https://github.com/wfschrad'));
+linkedIn.addEventListener('click', () => window.open('https://www.linkedin.com/in/william-schrader-7ba014199/'));
